412b26794b99ae18b3b75dad9aef48ee04712824
Rahul <rahulbiswalnew@gmail.com>
2023-02-01 18:24:08 +0530

    fifth  commit

--- script.js
+++ script.js
@@ -15,11 +15,17 @@ const getWeather = (city)=>{
 			console.log(response);
 			// cloud_pct.innerHTML = response.cloud_pct
 			temp.innerHTML = response.temp
+			temp2.innerHTML = response.temp
+
 			feels_like.innerHTML = response.feels_like
 			humidity.innerHTML = response.humidity
+			humidity2.innerHTML = response.humidity
+
 			min_temp.innerHTML = response.min_temp
     		max_temp.innerHTML = response.max_temp
     		wind_speed.innerHTML = response.wind_speed
+    		wind_speed2.innerHTML = response.wind_speed
+
 			wind_degrees.innerHTML = response.wind_degrees
 			sunrise.innerHTML = response.sunrise
     		sunset.innerHTML = response.sunset
