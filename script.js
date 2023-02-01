0204a3d4e507f830330343efc8da50462efd4283
Rahul <rahulbiswalnew@gmail.com>
2023-02-01 15:06:31 +0530

    third commit

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '2541a96c6dmsh2f1a4b585ae54e1p17a4c9jsn618affeb9d29',
-		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
-	}
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "2541a96c6dmsh2f1a4b585ae54e1p17a4c9jsn618affeb9d29",
+    "X-RapidAPI-Host": "weather-by-api-ninjas.p.rapidapi.com",
+  },
 };
 
-fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Delhi', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+const getWeather = (city)=>{ 
+	cityName.innerHTML = city;
+	fetch(
+		"https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=bhubaneswar",options)
+		.then((response) => response.json())
+		.then((response) => {
+			console.log(response);
+			cloud_pct.innerHTML = response.cloud_pct
+			temp.innerHTML = response.temp
+			feels_like.innerHTML = response.feels_like
+			humidity.innerHTML = response.humidity
+			min_temp.innerHTML = response.min_temp
+    		max_temp.innerHTML = response.max_temp
+    		wind_speed.innerHTML = response.wind_speed
+			wind_degrees.innerHTML = response.wind_degrees
+			sunrise.innerHTML = response.sunrise
+    		sunset.innerHTML = response.sunset
+		})
+		.catch((err) => console.error(err));
+	}
 
+submit.addEventListener("click", (e)=>{
+	// e.preventDefault()
+	getWeather(city.value);
+})
+
+getWeather("Bhubaneswar");
